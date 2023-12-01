0434cd7f2c27a76ae4eff9029a789a0ef7d06224
Ryan Lewis <ryan.lewis@lhv.com>
2023-12-01 06:24:08 +0000

    Day 01 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { line ->
+            line.toCharArray().map { c -> c.digitToIntOrNull() }.filterNotNull()
+        }
+
+        return numbers.sumOf { it.first() * 10 + it.last() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.map {
+            it.toNumberList()
+        }
+        return numbers.sumOf { it.first() * 10 + it.last() }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
+
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,38 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Converts string to list of numbers. Considers worded numbers as well (.e.g. "one" -> 1).
+ *
+ * Note: eightwo -> [8, 2]
+ */
+fun String.toNumberList(): List<Int> {
+    val m = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
+    val numbers = mutableListOf<Int>()
+    for (i in indices) {
+        val part = this.substring(i)
+
+        if (part.first().isDigit()) {
+            numbers.add(part.first().digitToInt())
+            continue
+        }
+
+        val key = m.keys.find { part.startsWith(it) }
+        if (key != null) {
+            numbers.add(m[key]!!)
+        }
+    }
+
+    return numbers
+}
